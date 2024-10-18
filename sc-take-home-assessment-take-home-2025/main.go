1712d6cdb50962711f88497322aeaf95bd92eae8
nikkichins <qinnikki2002@gmail.com>
2024-10-18 11:22:38 +1100

    Changed main method back to initial state

--- sc-take-home-assessment-take-home-2025/main.go
+++ sc-take-home-assessment-take-home-2025/main.go
@@ -8,19 +8,14 @@ import (
 )
 
 func main() {
-	// orgID := uuid.FromStringOrNil(folder.DefaultOrgID)
+	orgID := uuid.FromStringOrNil(folder.DefaultOrgID)
 	res := folder.GetAllFolders()
 
 	// example usage
 	folderDriver := folder.NewDriver(res)
-	res2, err := folderDriver.GetAllChildFolders(uuid.FromStringOrNil("c1556e17-b7c0-45a3-a6ae-9546248fb17a"), "sacred-moonstar")
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-	folder.PrettyPrint(res2)
-	// orgFolder := folderDriver.GetFoldersByOrgID(orgID)
+	orgFolder := folderDriver.GetFoldersByOrgID(orgID)
 
-	// folder.PrettyPrint(res)
-	// fmt.Printf("\n Folders for orgID: %s", orgID)
-	// folder.PrettyPrint(orgFolder)
+	folder.PrettyPrint(res)
+	fmt.Printf("\n Folders for orgID: %s", orgID)
+	folder.PrettyPrint(orgFolder)
 }
